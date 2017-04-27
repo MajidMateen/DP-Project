07006e4c137a5cf5d017455908b10e17ce37dd29
Majid mateen <Majid mateen@Majid-mateen>
2017-04-27 15:28:27 +0500

    cc

--- src/Customer.java
+++ src/Customer.java
@@ -5,23 +5,22 @@ public class Customer {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		
-//I HAD UPLOADED IT ON JENKINS
+		// I HAD UPLOADED IT ON JENKINS
+		// new things are happening...
 		DealBuilder dealbuilder = new DealBuilder();
-		  
+
 		System.out.println("Press 4 for getting pizza prices");
 		System.out.println(" Pizza's Deal");
 		System.out.println("Press 1 for NonVegitable Pizza");
 		System.out.println("Press 2 for Vegitable Pizza");
 		System.out.println("Press 3 for Select differnt  Pizza Flavor");
 		int input = scanner.nextInt();
-		
-		if(input ==3){
-			
-	         IPizza pizza=new BarBQ_Pizza(new FajitaPizza (new FajitaPizza(new PizzaBase())));
-			System.out.println("total price:"+pizza.getPrize());
-		}
 
+		if (input == 3) {
+
+			IPizza pizza = new BarBQ_Pizza(new FajitaPizza(new FajitaPizza(new PizzaBase())));
+			System.out.println("total price:" + pizza.getPrize());
+		}
 
 		if (input == 1) {
 
@@ -33,32 +32,22 @@ public class Customer {
 
 		} else if (input == 2) {
 
-			
-
 			Deal veg = dealbuilder.preapareVegitable();
 			System.out.println("Vegitable Pizza Deal");
 			veg.showItems();
-			
-			
-			
-			
-			
 
 		}
-		
-		else if(input==4)
-		{
-			Chain chainCallFajita=new FajitaPrice();
-			Chain chainCallTikka=new ChickenTikka();
-			Chain chainCallBeef=new BeefTikka();
-			
+
+		else if (input == 4) {
+			Chain chainCallFajita = new FajitaPrice();
+			Chain chainCallTikka = new ChickenTikka();
+			Chain chainCallBeef = new BeefTikka();
+
 			chainCallFajita.setNextChain(chainCallTikka);
 			chainCallTikka.setNextChain(chainCallBeef);
-			
-			
-			
-			PizzaSelector ps=new PizzaSelector("SmallPizza","FajitaPizza");
-		
+
+			PizzaSelector ps = new PizzaSelector("SmallPizza", "FajitaPizza");
+
 			chainCallFajita.calculate(ps);
 		}
 
